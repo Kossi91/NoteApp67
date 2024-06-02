581b076b3c8713bb797330f32870b31fb4c331c1
Aziz <kossi.91.ai@gmail.com>
2024-06-02 19:07:55 +0600

    Lesson 8

--- app/src/main/java/com/example/noteapp67/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/noteapp67/ui/activity/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.noteapp67.ui.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.noteapp67.R
 
 class MainActivity : AppCompatActivity() {
